f5ed51142868c45227958a9088bc2339cc40270b
Ryan McNeil <ryan.mcneil288@gmail.com>
2018-08-19 19:20:28 -0600

    Add Deck class and part of round_test

---
+++ test/round_test.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/guess'
+
+class GuessTest < Minitest::Test
+
+  def test_it_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_instance_of Round, round
+  end
+
+  def test_it_has_a_deck
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal deck, round.deck
+  end
+
+  def test_it_has_guesses
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal [], round.guesses
+  end
+
+  def test_it_can_determine_current_card
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal card_1, round.current_card
+  end
+
+  def test_it_can_record_guess
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal card_1, round.current_card
+  end
+end
